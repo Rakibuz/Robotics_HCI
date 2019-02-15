4a007a332fdfaf60a0bad8149a83a9c059cacf45
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2019-02-16 01:53:06 +0600

    Add files via upload

---
+++ Camera_Stream_c#/IPCamera/IPCamera/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using System.Web;
+using System.Net;
+using System.IO.Ports;
+namespace IPCamera
+{
+    public partial class Form1 : Form
+    {
+        MJPEGStream stream;
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+             stream = new MJPEGStream("http://192.168.0.2:8080/video");
+           // stream = new MJPEGStream("http://192.168.0.105:8080/video");
+            stream.NewFrame += stream_NewFrame;
+
+        }
+
+        private void stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // throw new NotImplementedException();
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stream.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stream.Stop();
+        }
+    }
+}
