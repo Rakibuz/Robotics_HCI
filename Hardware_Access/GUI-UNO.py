584e1ab55643f6eacd3a322e6a9f630491a54a74
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-06-04 14:38:14 +0600

    Update GUI-UNO.py

--- Hardware_Access/GUI-UNO.py
+++ Hardware_Access/GUI-UNO.py
@@ -1,29 +1,54 @@
-
 # Importing tkinter module
 from tkinter import *  
 import serial  
+import warnings
+import serial.tools.list_ports
+from tkinter import messagebox
+import pyttsx3
 
 
-Arduino_Serial = serial.Serial('COM4',9600) 
- 
+speech = pyttsx3.init()
+
 # Creating a tkinter window
 root = Tk()
- 
+
 # Initialize tkinter window with dimensions 300 x 250            
 root.geometry('300x250')    
- 
+
 
 # Creating a Button
 btn_on = Button(root, text = 'ON',width=5,height=3)
 # Set the position of button  
 btn_on.place(x=100, y=20)
- 
- 
+
+
 btn_off= Button(root, text = 'OFF',width=5,height=3)
 # Set the position of button 
 btn_off.place(x=100, y=100)
- 
- 
+
+
+
+arduino_ports = [
+    p.device
+    for p in serial.tools.list_ports.comports()
+    if 'Arduino' in p.description  # may need tweaking to match new arduinos
+]
+if not arduino_ports:
+    speech.say('No Arduino Found, Connect Arduino')
+    speech.runAndWait()
+    messagebox.showwarning('Info','No Arduino Found')
+if len(arduino_ports)== 1:
+    print(arduino_ports)
+    id=0
+    speech.say('Arduino Found at'+arduino_ports[id])
+    speech.runAndWait()
+if len(arduino_ports) > 1:
+    messagebox.showwarning('Info','Multiple Arduinos found - using the first')
+    id=0
+
+
+Arduino_Serial = serial.Serial(arduino_ports[id],9600)    
+
 # Defining method on click
 def On_Clicked(event):
     Arduino_Serial.write('1'.encode()) 
