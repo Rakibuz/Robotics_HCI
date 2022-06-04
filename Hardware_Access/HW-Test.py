18c0fcba84564adc851967fc4e396847aff7d5a3
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-06-04 12:01:53 +0600

    Update HW-Test.py

--- Hardware_Access/HW-Test.py
+++ Hardware_Access/HW-Test.py
@@ -1,42 +1,18 @@
+import serial.tools.list_ports
+# from tkinter import messagebox
 
-# Importing tkinter module
-from tkinter import *  
-import serial  
-
-
-Arduino_Serial = serial.Serial('COM4',9600) 
  
-# Creating a tkinter window
-root = Tk()
+# comlist = serial.tools.list_ports.comports()
+# connected = []
+# for element in comlist:
+#     connected.append(element.device)
  
-# Initialize tkinter window with dimensions 300 x 250            
-root.geometry('300x250')    
- 
+# messagebox.showinfo("Available COM Ports","" + str(connected) )
 
-# Creating a Button
-btn_on = Button(root, text = 'ON',width=5,height=3)
-# Set the position of button  
-btn_on.place(x=100, y=20)
+import win32com.client
+wmi = win32com.client.GetObject("winmgmts:")
+for serial in wmi.InstancesOf("Win32_SerialPort"):
+       print (serial.Name, serial.Description)
+
  
  
-btn_off= Button(root, text = 'OFF',width=5,height=3)
-# Set the position of button 
-btn_off.place(x=100, y=100)
- 
- 
- 
-# Defining method on click
-def On_Clicked(event):
-    Arduino_Serial.write('1'.encode()) 
-
-def Off_Clicked(event):
-    Arduino_Serial.write('0'.encode()) 
-
-btn_on.bind("<Button-1>" ,On_Clicked)
-#btn_on.pack()
-
-btn_off.bind("<Button-2>",Off_Clicked)
-#btn_off.pack()
-
-
-root.mainloop()
