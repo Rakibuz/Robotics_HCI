7dc82ef13ae28b1abd3f6e19582e2e0e1365e3d2
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-06-29 18:14:53 +0600

    Update Serial-to-console2.py

--- Hardware_Access/Serial-to-console2.py
+++ Hardware_Access/Serial-to-console2.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 
 print('Make sure you have selected correct COM & Baud rate')
 # make sure the 'COM#' is set according the Windows Device Manager
-ser = serial.Serial('COM5', 115200, timeout=1)
+ser = serial.Serial('COM13', 9600, timeout=1)
 time.sleep(2)
 
  
 for i in range(50):
     line = ser.readline()   # read a byte string
     if line:
-        string = line.decode()  # convert the byte string to a unicode string
+        com_string = line.decode()  # convert the byte string to a unicode string
         #num = int(string) # convert the unicode string to an int
-        print(string)
-         
+        print(com_string)
+        if "Tag UID" in com_string:
+            UID=com_string.split(':',2)
+            #print(UID[1])
+
 ser.close()
