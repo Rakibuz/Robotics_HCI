bdf563f3338a7915c83ed296f115cd0112f1887f
Md. Rakibuz Sultan Pranto <pranto055@gmail.com>
2022-12-06 16:59:27 +0600

    Create IR-receiver.py

---
+++ Raspberry Pi/IR-receiver.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+
+IrPin =11
+count=0
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)      
+    GPIO.setup(IrPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+
+def cnt(ev=None):
+    global count
+    count +=1
+    print("Received infrared. cnt= ", count)
+
+
+def loop():
+    GPIO.add_event_detect(IrPin,GPIO.FALLING, callback=cnt)
+    while True:
+        pass
+
+def destroy():
+    GPIO.cleanup()
+
+if __name__== '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+
